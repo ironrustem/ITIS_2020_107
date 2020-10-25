7e32de043c14cb33828029633db303ab0285a238
Rustem <ironrustem@gmail.com>
2020-10-25 17:21:29 +0300

    saifullin

--- Contacts/Contacts/AddContactViewController.swift
+++ Contacts/Contacts/AddContactViewController.swift
@@ -9,41 +9,44 @@ import UIKit
 
 class AddContactViewController: UIViewController {
 
-    @IBOutlet weak var doneButton: UIButton!
-    @IBOutlet weak var cancelButton: UIButton!
-    @IBOutlet weak var firstName: UITextField!
-    @IBOutlet weak var name: UITextField!
-    @IBOutlet weak var company: UITextField!
-    @IBOutlet weak var number: UITextField!
-    
-    var mainViewController:UIViewController!
-    
-    var timer1 : Timer?
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        timer1 = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(colorButtonDone), userInfo: nil, repeats: true)
-       
-    }
+  @IBOutlet weak var doneButton: UIButton!
+  @IBOutlet weak var cancelButton: UIButton!
+  @IBOutlet weak var firstName: UITextField!
+  @IBOutlet weak var name: UITextField!
+  @IBOutlet weak var company: UITextField!
+  @IBOutlet weak var number: UITextField!
 
-    @IBAction func cancel(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+  var mainViewController: UIViewController!
+
+  var timer1: Timer?
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    timer1 = Timer.scheduledTimer(
+      timeInterval: 0.5, target: self, selector: #selector(colorButtonDone), userInfo: nil,
+      repeats: true)
+
+  }
+
+  @IBAction func cancel(_ sender: Any) {
+    self.dismiss(animated: true, completion: nil)
+  }
+  @IBAction func done(_ sender: Any) {
+    if doneButton.isEnabled {
+      ContactsClass.share.addContact(
+        name: name.text!, surname: firstName.text!, company: company.text!, numbers: [number.text!])
+      mainViewController.viewWillAppear(true)
+      timer1?.invalidate()
+      self.dismiss(animated: true, completion: nil)
     }
-    @IBAction func done(_ sender: Any) {
-        if(doneButton.isEnabled){
-            ContactsClass.share.addContact(name: name.text!, surname: firstName.text!, company: company.text!, numbers: [number.text!])
-            mainViewController.viewWillAppear(true)
-            timer1?.invalidate();
-            self.dismiss(animated: true, completion: nil)
-        }
+  }
+
+  @objc func colorButtonDone() {
+    print(111111)
+    if (name.text == "") && (firstName.text == "") {
+      doneButton.isEnabled = false
+    } else {
+      doneButton.isEnabled = true
     }
-    
-    @objc func colorButtonDone(){
-        print(111111)
-        if (name.text == "")&&(firstName.text == "") {
-            doneButton.isEnabled = false
-        } else {
-            doneButton.isEnabled = true
-        }
-    }
+  }
 }
--- Contacts/Contacts/DetailViewController.swift
+++ Contacts/Contacts/DetailViewController.swift
@@ -9,29 +9,25 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var firstName: UILabel!
-    @IBOutlet weak var name: UILabel!
-    @IBOutlet weak var company: UILabel!
-    @IBOutlet weak var number: UILabel!
-    
-    var firstName1: String!
-    var name1: String!
-    var company1 : String!
-    var number1 : String!
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        firstName.text = firstName1
-        name.text = name1
-        company.text = company1
-        number.text = number1
+  @IBOutlet weak var firstName: UILabel!
+  @IBOutlet weak var name: UILabel!
+  @IBOutlet weak var company: UILabel!
+  @IBOutlet weak var number: UILabel!
 
-    }
-    
+  var firstName1: String!
+  var name1: String!
+  var company1: String!
+  var number1: String!
 
-    
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
+    firstName.text = firstName1
+    name.text = name1
+    company.text = company1
+    number.text = number1
+
+  }
 
 }
+
